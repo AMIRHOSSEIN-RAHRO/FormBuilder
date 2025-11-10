841e84e2948b9733885740fe8da08b787ffdd8ab
AMIRHOSSEIN RAHRO <my.amirhossein.rahro@gmail.com>
2025-11-10 16:12:50 +0330

    Update create-form.blade.php

--- forms/create-form.blade.php
+++ forms/create-form.blade.php
@@ -7,7 +7,7 @@
             @method($method)
         @endif
 
-        @if ($countInput > 1)
+        @if ($countInput > 0)
 
             @for ($i = 0; $i < $countInput; $i++)
                 @php $startNameNumberPK++; @endphp
@@ -22,19 +22,8 @@
                         class="  w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-red-500">
                 </div>
             @endfor
-        @else
-            <div class="Part_{{ $startNameNumberPK }}">
-                <label for="Part_{{ $startNameNumberPK }}_Input"
-                    class="block text-gray-700 font-semibold mb-1">{{ $propertyTitleArray[0] ?? '' }}</label>
-                <input value="{{ old('Part_' . $startNameNumberPK . '_Input',$oldArray[0] ?? '' ) }}"
-                    {{ ($propertyInputRequiredArray[0] ?? false) === true ? 'required' : '' }}
-                    type="{{ $propertyInputTypeArray[0] ?? 'text' }}" name="Part_{{ $startNameNumberPK }}_Input"
-                    id="Part_{{ $startNameNumberPK }}_Input"
-                    placeholder="{{ $propertyInputPlaceholderArray[0] ?? '' }}"
-                    class=" w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-red-500">
-            </div>
         @endif
-        <!-- usefully -->
+        <!-- usefully -> slot-->
         {{ $slot }}
         <!-- send -->
         <button type="submit" id="Part_{{ $startNameNumberPK }}_Submit" name="Part_{{ $startNameNumberPK }}_Submit"

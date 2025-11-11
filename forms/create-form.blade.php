a316b53751415e1ef95e0746b7e028441a5b30a2
AMIRHOSSEIN RAHRO <my.amirhossein.rahro@gmail.com>
2025-11-11 19:25:54 +0330

    Update create-form.blade.php

--- forms/create-form.blade.php
+++ forms/create-form.blade.php
@@ -8,15 +8,15 @@
     @if ($countInput > 0)
         @for ($i = 0; $i < $countInput; $i++)
 
-              @php if ($autoNameId===true) $startNameNumberPK++; else $startNameNumberPK=false; @endphp
+              @php if ($autoNameId===true) $autoIdStart++; else $autoIdStart=false; @endphp
 
-                <div class="{{ $startNameNumberPK !== false ? 'Part_'.$startNameNumberPK : $idArray[$i] }}">
-                  <label for="{{  $startNameNumberPK !== false ? 'Part_'.$startNameNumberPK.'_Input' : $nameArray[$i] }}"
+                <div class="{{ $autoIdStart !== false ? 'Part_'.$autoIdStart : $idArray[$i] }}">
+                  <label for="{{  $autoIdStart !== false ? 'Part_'.$autoIdStart.'_Input' : $nameArray[$i] }}"
                          class="block text-gray-700 font-semibold mb-1">{{ $propertyTitleArray[$i] ?? '' }}</label>
-                  <input value="{{ old(($startNameNumberPK !== false ? 'Part_'.$startNameNumberPK .'_Input' : $nameArray[$i]), $oldArray[$i] ?? '') }}"
+                  <input value="{{ old(($autoIdStart !== false ? 'Part_'.$autoIdStart .'_Input' : $nameArray[$i]), $oldArray[$i] ?? '') }}"
                          type="{{ $propertyInputTypeArray[$i] ?? 'text' }}" {{ ($propertyInputRequiredArray[$i] ?? false) === true ? 'required' : '' }}
-                         name="{{  $startNameNumberPK !== false ? 'Part_'.$startNameNumberPK.'_Input' : $nameArray[$i] }}"
-                         id="{{ $startNameNumberPK !== false ? 'Part_'.$startNameNumberPK.'_Input' : $idArray[$i] }}"
+                         name="{{  $autoIdStart !== false ? 'Part_'.$autoIdStart.'_Input' : $nameArray[$i] }}"
+                         id="{{ $autoIdStart !== false ? 'Part_'.$autoIdStart.'_Input' : $idArray[$i] }}"
                          placeholder="{{ $propertyInputPlaceholderArray[$i] ?? '' }}"
                          class="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-red-500">
                 </div>
@@ -27,9 +27,9 @@
         <!-- usefully -> slot-->   {{ $slot }}
 
         <!-- submit -->
-        @php if ($autoNameId===true) $startNameNumberPK++;  @endphp
-        <button type="submit" id="{{ empty($idSubmit) ? 'Part_'.$startNameNumberPK.'_Submit' : $idSubmit }}"
-                name="{{ empty($nameSubmit) ? 'Part_'.$startNameNumberPK.'_Submit' : $nameSubmit }}"
+        @php if ($autoNameId===true) $autoIdStart++;  @endphp
+        <button type="submit" id="{{ empty($idSubmit) ? 'Part_'.$autoIdStart.'_Submit' : $idSubmit }}"
+                name="{{ empty($nameSubmit) ? 'Part_'.$autoIdStart.'_Submit' : $nameSubmit }}"
                 class="bg-red-500 text-white px-6 py-2 rounded hover:bg-red-600 transition">
             {{ $submitText }}
         </button>
